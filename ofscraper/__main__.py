ee59d3acf75c56b9d28b0da7b3d667b66e24cd3a
Harry Kakoulidis <wicsatenemuzz@outlook.com>
2026-01-02 13:26:12 -0600

    update

---
+++ ofscraper/__main__.py
@@ -0,0 +1,17 @@
+#!/root/OF-Scraper/.venv/bin/python
+# -*- coding: utf-8 -*-
+import ofscraper.utils.system.init
+import multiprocessing
+
+import ofscraper.main.open.load as load
+import ofscraper.utils.system.system as system
+
+
+def main():
+    if system.get_parent():
+        load.main()
+
+
+if __name__ == "__main__":
+    multiprocessing.freeze_support()
+    main()
