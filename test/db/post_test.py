fbd3f61f226a2aa6568b9d747c60a01c3a42948f
Harry Kakoulidis <wicsatenemuzz@outlook.com>
2026-01-02 13:40:04 -0600

    update

---
+++ test/db/post_test.py
@@ -0,0 +1,77 @@
+import tempfile
+
+from ofscraper.classes.posts import Post
+from ofscraper.const.test_constants import *
+from ofscraper.db.operations import *
+from ofscraper.utils.paths.paths import cleanDB
+
+
+def test_post_create(mocker):
+    cleanDB()
+    with tempfile.NamedTemporaryFile() as p:
+        try:
+            a = mocker
+            a.trace = None
+            mocker.patch(
+                "ofscraper.classes.placeholder.Placeholders.databasePathHelper",
+                return_value=pathlib.Path(p.name),
+            )
+            mocker.patch(
+                "ofscraper.classes.placeholder.Placeholders.databasePathCopyHelper",
+                return_value=pathlib.Path(p.name),
+            )
+            mocker.patch("ofscraper.db.operations.log", new_callabe=a)
+            mocker.patch("ofscraper.db.operations.FileLock.acquire", return_value=True)
+            mocker.patch("ofscraper.db.operations.FileLock.release", return_value=True)
+            create_post_table("11111", "test")
+        except:
+            raise Exception
+
+
+# def test_post_failure(mocker):
+#     cleanDB()
+#     with tempfile.NamedTemporaryFile() as p:
+#         with pytest.raises(Exception):
+#             a=mocker
+#             a.trace=None
+#             mocker.patch("ofscraper.classes.placeholder.Placeholders.databasePathHelper",return_value=pathlib.Path(p.name))
+#             mocker.patch("ofscraper.classes.placeholder.Placeholders.databasePathCopyHelper",return_value=pathlib.Path(p.name))
+#             mocker.patch("ofscraper.db.operations.log",new_callabe=a)
+#             create_post_table("11111")
+
+
+def test_post_insert(mocker):
+    cleanDB()
+    with tempfile.NamedTemporaryFile() as p:
+        try:
+            a = mocker
+            a.trace = None
+            mocker.patch(
+                "ofscraper.classes.placeholder.Placeholders.databasePathHelper",
+                return_value=pathlib.Path(p.name),
+            )
+            mocker.patch(
+                "ofscraper.classes.placeholder.Placeholders.databasePathCopyHelper",
+                return_value=pathlib.Path(p.name),
+            )
+            mocker.patch("ofscraper.db.operations.log", new_callabe=a)
+            mocker.patch("ofscraper.db.operations.FileLock.acquire", return_value=True)
+            mocker.patch("ofscraper.db.operations.FileLock.release", return_value=True)
+            create_post_table("11111", "test")
+            write_post_table(Post(TIMELINE_EXAMPLE, "11111", "test"), "11111", "test")
+        except Exception as E:
+            print(E)
+            raise Exception
+
+
+# def test_post_insert_failure(mocker):
+#     cleanDB()
+#     with tempfile.NamedTemporaryFile() as p:
+#         with pytest.raises(Exception):
+#             a=mocker
+#             a.trace=None
+#             mocker.patch("ofscraper.classes.placeholder.Placeholders.databasePathHelper",return_value=pathlib.Path(p.name))
+#             mocker.patch("ofscraper.classes.placeholder.Placeholders.databasePathCopyHelper",return_value=pathlib.Path(p.name))
+#             mocker.patch("ofscraper.db.operations.log",new_callabe=a)
+#             create_post_table("11111","test")
+#             write_post_table(Post(TIMELINE_EXAMPLE,"111"))
