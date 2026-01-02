d92d674fadf56f4770cc314d59ab501335cc311d
Harry Kakoulidis <wicsatenemuzz@outlook.com>
2026-01-02 13:32:19 -0600

    update

---
+++ ofscraper/utils/const.py
@@ -0,0 +1,10 @@
+KEY_OPTIONS = ["cdrm", "manual"]
+DYNAMIC_OPTIONS = [
+    "legacy",
+    "manual",
+    "generic",
+    "standard",
+    "extended",
+    "custom",
+]
+METADATA_OPTIONS = ["complete", "update", "check"]
