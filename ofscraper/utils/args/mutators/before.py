4404191ac5fd86b2e7d3df684c46848d86e3ecfc
Harry Kakoulidis <wicsatenemuzz@outlook.com>
2026-01-02 13:33:26 -0600

    update

---
+++ ofscraper/utils/args/mutators/before.py
@@ -0,0 +1,11 @@
+import arrow
+
+
+def update_before():
+    import ofscraper.utils.settings as settings
+
+    args = settings.get_args()
+    if args.before_original:
+        return
+    args.before = arrow.now().shift(days=4)
+    settings.update_args(args)
