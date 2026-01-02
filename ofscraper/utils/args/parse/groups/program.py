0e33608f8ee87ad440f94125512b313a607469e0
Harry Kakoulidis <wicsatenemuzz@outlook.com>
2026-01-02 13:35:00 -0600

    update

---
+++ ofscraper/utils/args/parse/groups/program.py
@@ -0,0 +1,23 @@
+import cloup as click
+
+from ofscraper.utils.args.parse.arguments.program import (
+    config_location_option,
+    profile_option,
+    version_option,
+    env_file_option,
+)
+
+program_options_desc = "Program Options"
+program_options_help = "Control the application's behavior with these settings"
+program_options_tuple = (
+    version_option,
+    config_location_option,
+    profile_option,
+    env_file_option,
+)
+# Create the option group
+program_options = click.option_group(
+    program_options_desc,
+    *program_options_tuple,
+    help=program_options_help,
+)
