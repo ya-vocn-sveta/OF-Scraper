e6c3a063f0c907e687be18b9fc0f6640c12b1d0b
Harry Kakoulidis <wicsatenemuzz@outlook.com>
2026-01-02 13:30:39 -0600

    update

---
+++ ofscraper/main/close/final/final.py
@@ -0,0 +1,22 @@
+r"""
+
+ _______  _______         _______  _______  _______  _______  _______  _______  _______
+(  ___  )(  ____ \       (  ____ \(  ____ \(  ____ )(  ___  )(  ____ )(  ____ \(  ____ )
+| (   ) || (    \/       | (    \/| (    \/| (    )|| (   ) || (    )|| (    \/| (    )|
+| |   | || (__     _____ | (_____ | |      | (____)|| (___) || (____)|| (__    | (____)|
+| |   | ||  __)   (_____)(_____  )| |      |     __)|  ___  ||  _____)|  __)   |     __)
+| |   | || (                   ) || |      | (\ (   | (   ) || (      | (      | (\ (
+| (___) || )             /\____) || (____/\| ) \ \__| )   ( || )      | (____/\| ) \ \__
+(_______)|/              \_______)(_______/|/   \__/|/     \||/       (_______/|/   \__/
+                                                                                      f
+"""
+
+from ofscraper.scripts.final_script import final_script
+from ofscraper.utils.logs.logs import printEndValues
+import ofscraper.managers.manager as manager
+
+
+def final_action():
+    final_script()
+    manager.Manager.stats_manager.print_summary_by_activity()
+    printEndValues()
