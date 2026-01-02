d7600611f78835c30bd5e4cda0fb95caea5554c4
Harry Kakoulidis <wicsatenemuzz@outlook.com>
2026-01-02 13:34:12 -0600

    update

---
+++ ofscraper/utils/args/parse/arguments/utils/date.py
@@ -0,0 +1,13 @@
+import arrow
+
+
+def before_callback(ctx, param, value):
+    params = ctx.params
+    if not value:
+        params[f"{param.name}_original"] = value
+        params[f"{param.name}"] = arrow.get(value or arrow.now()).shift(days=4)
+        return params[f"{param.name}"]
+    else:
+        params[f"{param.name}_original"] = value
+        params[f"{param.name}"] = value
+        return params[f"{param.name}"]
