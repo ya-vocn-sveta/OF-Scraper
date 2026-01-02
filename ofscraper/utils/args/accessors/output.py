25b6cf7e5a06996bf0577fa5c3bc5cbe6fe8c831
Harry Kakoulidis <wicsatenemuzz@outlook.com>
2026-01-02 13:32:56 -0600

    update

---
+++ ofscraper/utils/args/accessors/output.py
@@ -0,0 +1,8 @@
+import ofscraper.utils.settings as settings
+
+
+def low_output():
+    return (
+        settings.get_args().output in {"OFF", "LOW", "PROMPT"}
+        or settings.get_args().no_rich
+    )
